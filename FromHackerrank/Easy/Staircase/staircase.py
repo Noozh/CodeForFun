58bc2b3902e087e2f7e6a32b8c150d686e1a68d8
Daniel Arevalo Rodrigo <daniel@alpha21.aulas.gsyc.urjc.es>
2019-02-15 15:10:38 +0100

    This program prints an arbitrary stair using #

--- FromHackerrank/Easy/Staircase/staircase.py
+++ FromHackerrank/Easy/Staircase/staircase.py
@@ -1,12 +1,28 @@
 
+#!/bin/python3
+
+"""
+Consider a staircase of size n = 4:
+
+   #
+  ##
+ ###
+####
+
+Observe that its base and height are both equal to, and the image is drawn
+using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size n
+"""
+
 n = int(input())
-k = 0
-i = n-1
-for j in range (0,n):
-    for k in range(k,n-1):
+k = 1
+p = n
+for k in range (1,n+1):
+    for j in range(1,p):
         print(" ", sep='', end='')
-    for i in range (n,i):
+    for i in range (0,k):
         print("#", sep='', end='')
-    i = i -1
+    p = p -1
     k = k +1
     print()
